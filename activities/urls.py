3d184848ad32344a0e824a13e89e2f4581091e56
Sirapob Phasut <omegaooh@gmail.com>
2024-10-02 21:05:27 +0700

    create index view for activity

---
+++ activities/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for activities app.
+"""
+from django.urls import path
+from . import views
+
+app_name = "activities"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+
+]
--- activities/views.py
+++ activities/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+from . import models
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.ListView):
+    """View class to show all upcoming activities"""
+    model = models.Activity
+    template_name = "activities/index.html"
+    context_object_name = "activities"
+
+    def get_queryset(self):
+        """ 
+        Return Queryset of activities that is not took place yet.
+        
+        Queryset is order by date that the activity took place.(ealier to later)
+        """
+        return models.Activity.objects.filter(date__gte=timezone.now()).order_by("date")
+    
