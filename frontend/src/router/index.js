6d2f39177ac9a39b42745d423bede482dab416c9
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-10-05 20:33:45 +0700

    Edit files follow the jshint
    - Using jshint to lint javascript
    - Add Javadoc

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import ActivityIndex from '../views/ActivityIndex.vue';
-import ActivityDetail from '../views/ActivityDetail.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import ActivityIndex from "../views/ActivityIndex.vue";
+import ActivityDetail from "../views/ActivityDetail.vue";
 
+// Just like  urls.py
 const routes = [
-  {
-    path: '/',
-    name: 'ActivityIndexPage',
-    component: ActivityIndex,
-  },
-  {
-    path: '/activities/:id',
-    name: 'ActivityDetailPage',
-    component: ActivityDetail
-  },
+    {
+        path: "/",
+        name: "ActivityIndexPage",
+        component: ActivityIndex,
+    },
+    {
+        path: "/activities/:id",
+        name: "ActivityDetailPage",
+        component: ActivityDetail,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
--- frontend/src/utils/csrf.js
+++ frontend/src/utils/csrf.js
@@ -1,15 +1,20 @@
 export function getCsrfToken() {
+    /*
+     * Trying to get CSRF Token.
+     * @returns {string} CSRF Token.
+     */
     let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        if (cookie.startsWith('csrftoken=')) {
-          cookieValue = decodeURIComponent(cookie.substring('csrftoken='.length));
-          break;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.startsWith("csrftoken=")) {
+                cookieValue = decodeURIComponent(
+                    cookie.substring("csrftoken=".length)
+                );
+                break;
+            }
         }
-      }
     }
     return cookieValue;
-  }
-  
+}
