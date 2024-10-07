15089dd70149d8f20b1fb7a3eff7318bbdc4516e
Sirapob Phasut <71599562+OmegaOoh@users.noreply.github.com>
2024-10-07 18:21:47 +0700

    Add Vue Date Picker, Add settings for daisyui

--- frontend/src/views/ActivityCreate.vue
+++ frontend/src/views/ActivityCreate.vue
@@ -5,38 +5,38 @@
             <h2 class="class-title">Create Activity</h2>
             <label>Activity Name
             <input 
-            v-model="activityName"
-            type="text" 
-            placeholder="Activity Name" 
-            class="input input-bordered input-primary w-full max-w-xs mb-4" 
+                v-model="activityName"
+                type="text" 
+                placeholder="Activity Name" 
+                class="input input-bordered input-primary w-full max-w-xs mb-4" 
             />
             </label>
             <label>Activity Detail
             <textarea 
-            v-model="activityDetail"
-            class="textarea textarea-primary w-full max-w-xs mb-4" 
-            placeholder="Activity Detail">
+                v-model="activityDetail"
+                class="textarea textarea-primary w-full max-w-xs mb-4" 
+                placeholder="Activity Detail">
             </textarea>
             </label>
             <label>Date and Time
-            <input 
-            v-model="date"
-            type="text" 
-            datepicker 
-            class="input input-bordered input-primary w-full max-w-xs mb-4" 
-            placeholder="Select Date" 
+            <VueDatePicker 
+                v-model="date"
+                type="text"
+                placeholder="Select Date" 
+                :min-date="new Date()"
             />
             </label>
             <label>Max People
             <input 
-            v-model.number="maxPeople"
-            type="number" 
-            placeholder="Enter Max People (Optional)" 
-            class="input input-bordered input-primary w-full max-w-xs mb-4"
+                v-model.number="maxPeople"
+                type="number" 
+                placeholder="Enter Max People (Optional)" 
+                class="input input-bordered input-primary w-full max-w-xs mb-4"
+                :min="0"
             />
             </label>
             <button class="btn btn-primary" @click="logData">Create Activity</button>
-            <button class="btn btn-secondary">Back to List</button>
+            <button class="btn btn-secondary" @click="goBack">Back to List</button>
         </div>
     </div>
 </div>
@@ -54,7 +54,17 @@ export default {
         };
     },
     methods: {
+        goBack() {
+            /*
+             * Navigate back to Activity Index page.
+             * This function does not return anything.
+             */
+            this.$router.push("/");
+        },
         logData() {
+            if (this.max_people < 0) {
+                this.max_people = 0;
+            }
                 console.log('Activity Name:', this.activityName);
                 console.log('Activity Detail:', this.activityDetail);
                 console.log('Selected Date:', this.date);
