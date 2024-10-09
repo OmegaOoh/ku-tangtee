88b28173fe4e9cd9e933c54e6a9b3f245bb819b5
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-10-09 15:01:17 +0700

    Fixing flake8 and ignore mypy for this timezone function
    timezone function has different kind of types

--- activities/utils.py
+++ activities/utils.py
@@ -1,8 +1,10 @@
+"""Utility module that provides convenient functions."""
 import pytz
 from datetime import datetime
 from django.conf import settings
 
-def get_time_zone_offset() -> int:
+
+def get_time_zone_offset() -> int:  # type: ignore[no-untyped-def] ## pragma: no cover
     """Get the UTC offset for a given time zone."""
     try:
         # Get the timezone object
@@ -13,8 +15,12 @@ def get_time_zone_offset() -> int:
         # Get the local time in the specified timezone
         local_time = now_utc.astimezone(tz)
         # Calculate the offset in hours
-        offset = local_time.utcoffset().total_seconds() / 3600
-        return int(offset)
+        offset = local_time.utcoffset()
+
+        if offset is not None:
+            return int(offset.total_seconds() / 3600)
+        else:
+            return 0  # If utcoffset is None, return 0
     except Exception as e:
         print(f"Error getting offset for time zone {time_zone}: {e}")
         return 0  # Return 0 if there's an error
