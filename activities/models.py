9c15c4d779eab5c9039b960fe818145177ef0a3c
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-09 14:13:14 +0700

    Add attend class

--- activities/models.py
+++ activities/models.py
@@ -2,6 +2,7 @@
 from typing import Any
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Activity(models.Model):
@@ -11,7 +12,6 @@ class Activity(models.Model):
     detail = models.CharField(max_length=1024)
     date = models.DateTimeField(default=timezone.now)
     max_people = models.IntegerField(null=True, blank=True)
-    people = models.IntegerField(default=0)
 
     def __str__(self) -> models.CharField:
         """Return Activity Name as string representative."""
@@ -27,3 +27,28 @@ class Activity(models.Model):
     def is_upcoming(self) -> Any:
         """Return True if activities took place on incoming weeks, Otherwise false."""
         return timezone.now() + timezone.timedelta(weeks=1) >= self.date and self.can_join()
+
+    def host(self) -> User:
+        """Return host of an activity."""
+        return self.attend_set.filter(is_host=True)
+
+    @property
+    def people(self) -> int:
+        """Return number of people attend this activity include host."""
+        return self.attend_set.count()
+
+
+class Attend(models.Model):
+    """Attend model to store activity attendance."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    is_host = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        """Return user's first name and which activity they join."""
+        return f"user {self.user.first_name} attend {self.activity.name}"
+
+    def __repr__(self) -> str:
+        """Return user's first name and which activity they join."""
+        return str.__str__()
