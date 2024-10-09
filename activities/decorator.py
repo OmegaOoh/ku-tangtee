0abc63caecd0dc530dd258c8f3935a155a382b22
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-09 14:30:44 +0700

    Remove unneccessary decorator and import right type hint

--- activities/decorator.py
+++ activities/decorator.py
@@ -1,10 +1,10 @@
 """Custom decorator for activities app."""
 
 from django.http import HttpRequest, JsonResponse
-from typing import func
+from typing import Callable
 
 
-def login_required(func: func) -> func:
+def login_required(func: Callable) -> Callable:
     """Return a wrapper function that make function return error Json when user doesn't login."""
 
     def wrapper(request: HttpRequest) -> JsonResponse:
@@ -16,23 +16,6 @@ def login_required(func: func) -> func:
                 'error': 'User must authenticated before perform this action'
             },
             status=401
-        )
-
-    return wrapper
-
-
-def post_req_required(func: func) -> func:
-    """Return a wrapper function that make function return error Json when user doesn't login."""
-
-    def wrapper(request: HttpRequest) -> JsonResponse:
-        """If user already login just invoke request handler func."""
-        if request.method == "POST":
-            return func
-        return JsonResponse(
-            {
-                'error': 'Forbidden access'
-            },
-            status=403
         )
 
     return wrapper
--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,7 @@
 """Views for activities app, handle html request."""
 from django.http import HttpRequest, JsonResponse
-from django.contrib.auth.decorators import login_required
 
 
-@login_required
 def is_authen(request: HttpRequest) -> JsonResponse:
     """Return Json which inform that user is login right now."""
     print(request.user.is_authenticated)
