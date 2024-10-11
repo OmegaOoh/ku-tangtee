6649313afbe06eb6161b22012c7913a0e4c8cc5b
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-11 08:54:51 +0700

    Uncomment some test case

--- activities/tests/test_index.py
+++ activities/tests/test_index.py
@@ -27,29 +27,29 @@ class IndexTest(django.test.TestCase):
         ]
         self.assertJSONEqual(response.content, expected)
 
-    # def test_past_activity(self):
-    #     """Activities take places in the past showed on the index page."""
-    #     create_activity(host=self.host_user, days_delta=-1)
-    #     response = self.client.get(urls.reverse("activities:index"))
-    #     self.assertJSONEqual(response.content, [])
+    def test_past_activity(self):
+        """Activities take places in the past showed on the index page."""
+        create_activity(host=self.host_user, days_delta=-1)
+        response = self.client.get(urls.reverse("activities:index"))
+        self.assertJSONEqual(response.content, [])
 
-    # def test_future_and_past_activity(self):
-    #     """Only activities take place in the future is showed on index page."""
-    #     response, activity = create_activity(host=self.host_user)
-    #     create_activity(host=self.host_user, days_delta=-1)
-    #     response = self.client.get(urls.reverse("activities:index"))
-    #     expected = [
-    #         activity_to_json(activity)
-    #     ]
-    #     self.assertJSONEqual(response.content, expected)
+    def test_future_and_past_activity(self):
+        """Only activities take place in the future is showed on index page."""
+        response, activity = create_activity(host=self.host_user)
+        create_activity(host=self.host_user, days_delta=-1)
+        response = self.client.get(urls.reverse("activities:index"))
+        expected = [
+            activity_to_json(activity)
+        ]
+        self.assertJSONEqual(response.content, expected)
 
-    # def test_two_future_activity(self):
-    #     """Both of activity is showed on index page."""
-    #     response1, activity1 = create_activity(host=self.host_user)
-    #     response2, activity2 = create_activity(host=self.host_user)
-    #     response = self.client.get(urls.reverse("activities:index"))
-    #     expected = [
-    #         activity_to_json(activity1),
-    #         activity_to_json(activity2)
-    #     ]
-    #     self.assertJSONEqual(response.content, expected)
+    def test_two_future_activity(self):
+        """Both of activity is showed on index page."""
+        response1, activity1 = create_activity(host=self.host_user)
+        response2, activity2 = create_activity(host=self.host_user)
+        response = self.client.get(urls.reverse("activities:index"))
+        expected = [
+            activity_to_json(activity1),
+            activity_to_json(activity2)
+        ]
+        self.assertJSONEqual(response.content, expected)
