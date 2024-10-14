e5669179f605c9fd666c2e446ea5768699186e73
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-10-14 18:35:43 +0700

    Profile picture view in base template header
    - Dropdown feature when hover mouse in dropdown area (profile picture and name)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,11 +16,38 @@
                             Login Using Google
                         </button>
                     </div>
-                    <div v-else>
-                        Welcome, {{ fName }} {{ lName }}
-                        <button class="btn btn-primary" @click="logout">
-                            Logout
-                        </button>
+                    <div
+                        v-else
+                        class="relative"
+                        @mouseenter="showDropdown"
+                        @mouseleave="hideDropdown"
+                    >
+                        <div
+                            class="btn btn-primary flex items-center cursor-pointer"
+                        >
+                            <img
+                                v-if="pfp"
+                                :src="pfp"
+                                alt="Profile Picture"
+                                class="w-8 h-8 rounded-full mr-2"
+                            />
+                            <span>{{ fName }} {{ lName }}</span>
+                        </div>
+                        <div
+                            v-if="isDropdown"
+                            class="absolute right-0 w-48 bg-warning rounded-full shadow-lg z-10"
+                            @mouseenter="keepDropdownOpen"
+                            @mouseleave="checkHideDropdown"
+                        >
+                            <ul>
+                                <li
+                                    class="block px-4 py-2 text-sm hover:bg-error cursor-pointer rounded-full"
+                                    @click="logout"
+                                >
+                                    Logout
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -57,6 +84,9 @@ export default {
             fName: "",
             lName: "",
             isDarkTheme: false,
+            pfp: "",
+            isDropdown: false,
+            hideTimeout: null,
         };
     },
     mounted() {
@@ -74,17 +104,60 @@ export default {
         this.authStatus();
     },
     methods: {
+        showDropdown() {
+            /**
+             * Show dropdown.
+             * This function return nothing.
+             */
+            this.isDropdown = true;
+            this.isMouseOverDropdown = true;
+            clearTimeout(this.hideTimeout);
+        },
+        hideDropdown() {
+            /**
+             * Deactivate dropdown.
+             * This function return nothing.
+             */
+            this.isDropdown = false;
+            this.isMouseOverDropdown = false;
+        },
+        checkHideDropdown() {
+            /**
+             * Deactivate dropdown with timer and mouse condition.
+             * This function return nothing.
+             */
+            if (!this.isMouseOverDropdown) {
+                this.hideDropdown();
+            }
+        },
+        startHideTimeout() {
+            /**
+             * This sets a timer for hiding dropdown.
+             * This function returns nothing.
+             */
+            this.hideTimeout = setTimeout(() => {
+                this.checkHideDropdown();
+            }, 1000);
+        },
+        keepDropdownOpen() {
+            /**
+             * This sets a flag indicating the mouse is over the dropdown.
+             * This function returns nothing.
+             */
+            this.isMouseOverDropdown = true;
+            clearTimeout(this.hideTimeout);
+        },
         async login() {
             /**
              * Log user in with Google authentication token and set user data.
              * This function return nothing.
              */
-            try
-            {
-                const logInResponse = await googleTokenLogin()
-                const csrfToken = (await apiClient.get(`activities/get-csrf-token/`,{})).data.csrfToken;
-                if (this.isAuth) {            
-
+            try {
+                const logInResponse = await googleTokenLogin();
+                const csrfToken = (
+                    await apiClient.get(`activities/get-csrf-token/`, {})
+                ).data.csrfToken;
+                if (this.isAuth) {
                     await apiClient.post(
                         `rest-auth/logout/`,
                         {},
@@ -117,31 +190,35 @@ export default {
              * Check session authentication status
              * This function does not return anything.
              */
-            try { 
-                const csrfToken = (await apiClient.get(`activities/get-csrf-token/`,{})).data.csrfToken;
-                await apiClient.post(`rest-auth/token/verify/`,
+            try {
+                const csrfToken = (
+                    await apiClient.get(`activities/get-csrf-token/`, {})
+                ).data.csrfToken;
+                await apiClient.post(
+                    `rest-auth/token/verify/`,
                     {
-                        token: sessionStorage.getItem('token')
-                    }, 
+                        token: sessionStorage.getItem("token"),
+                    },
                     {
-                        headers: {"X-CsrfToken": csrfToken},
-                        withCredentials: true
+                        headers: { "X-CsrfToken": csrfToken },
+                        withCredentials: true,
                     }
-                )
+                );
                 this.isAuth = true;
                 this.getUserData();
-            }
-            catch{
+            } catch {
                 this.isAuth = false;
             }
         },
         async logout() {
             /**
-            * Logout user from the system.
-            * this function return nothing.
-            */
-            const csrfToken = (await apiClient.get(`activities/get-csrf-token/`,{})).data.csrfToken;
-            if (this.isAuth) {            
+             * Logout user from the system.
+             * this function return nothing.
+             */
+            const csrfToken = (
+                await apiClient.get(`activities/get-csrf-token/`, {})
+            ).data.csrfToken;
+            if (this.isAuth) {
                 await apiClient.post(
                     `rest-auth/logout/`,
                     {},
@@ -153,16 +230,20 @@ export default {
             }
             this.isAuth = false;
             sessionStorage.setItem("token", "");
+            this.isDropdown = false;
         },
         async getUserData() {
             /**
              * Get user data from backend.
              * this function return nothing.
              */
-            const response = await apiClient.get(`rest-auth/user/`)
-            console.log(response)
+            const response = await apiClient.get(`rest-auth/user/`);
+            console.log(response);
             this.fName = response.data.first_name;
             this.lName = response.data.last_name;
+            const profilePic = await apiClient.get(`profile-pic/`);
+            console.log(profilePic);
+            this.pfp = profilePic.data.profile_picture_url;
         },
     },
 };
