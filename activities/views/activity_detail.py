fc49868d4890969cf933e321d89f012c420605b9
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-15 21:13:05 +0700

    Add additional note to activity_detail.py

--- activities/views/activity_detail.py
+++ activities/views/activity_detail.py
@@ -5,6 +5,7 @@ TODO
     - If request is GET return activity detail that has id equal to activity_id.
     - If request is PUT update activity according to request content where it's id equal to activity_id.
     - Edit activity is limit to host of an activity therefore, we need permission class to enforce it.
+    - Fix _test_detail.py and test_edit_activity.py after create a ApiView class 
 - Refer to this docs
     Class base API view https://www.django-rest-framework.org/tutorial/3-class-based-views/
     Permission class https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/
