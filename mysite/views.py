68e0756cdb86fe7b8b4fd6b612e16d3dcc1806f7
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-15 21:49:38 +0700

    Convert to rest framework

--- mysite/views.py
+++ mysite/views.py
@@ -4,15 +4,17 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from allauth.socialaccount.models import SocialAccount
 from .settings import OAUTH_PROVIDER
+from rest_framework import decorators, response
 
 
-def profile_picture_view(request: HttpRequest) -> JsonResponse:  # pragma: no cover
+@decorators.api_view(["GET"])
+def profile_picture_view(request: HttpRequest) -> response.Response:  # pragma: no cover
     """Return profile picture url from Google account."""
     user = request.user
-    if request.method == 'GET':
-        try:
-            social_account = SocialAccount.objects.get(user=user, provider=OAUTH_PROVIDER)
-            profile_picture_url = social_account.extra_data.get('picture', '')
-            return JsonResponse({"profile_picture_url": profile_picture_url})
-        except SocialAccount.DoesNotExist:
-            return JsonResponse({"error": "Google account not found for user"}, status=404)
+    try:
+        social_account = SocialAccount.objects.get(user=user, provider=OAUTH_PROVIDER)
+        profile_picture_url = social_account.extra_data.get('picture', '')
+        return response.Response({"profile_picture_url": profile_picture_url})
+    except SocialAccount.DoesNotExist:
+        return response.Response({"error": "Google account not found for user"}, status=404)
+    
