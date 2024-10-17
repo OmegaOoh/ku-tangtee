128dbfb04dea9ef72a31a3d57f34ec4085f938b9
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-10-17 21:53:14 +0700

    Specify type hints in Activity model

--- activities/models.py
+++ activities/models.py
@@ -1,5 +1,4 @@
 """Database Model for activities app."""
-from typing import Any
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -13,18 +12,18 @@ class Activity(models.Model):
     date = models.DateTimeField(default=timezone.now)
     max_people = models.IntegerField(null=True, blank=True)
 
-    def __str__(self) -> models.CharField:
+    def __str__(self) -> str:
         """Return Activity Name as string representative."""
         return self.name
 
-    def can_join(self) -> Any:
-        """Return True if max_people doesn't reached and date doesn't past, Otherwise false."""
+    def can_join(self) -> bool:
+        """Return True if max_people doesn't reach and date doesn't past, Otherwise false."""
         if self.max_people:
             return self.date >= timezone.now() and self.people < self.max_people
         else:
             return self.date >= timezone.now()
 
-    def is_upcoming(self) -> Any:
+    def is_upcoming(self) -> bool:
         """Return True if activities took place on incoming weeks, Otherwise false."""
         return timezone.now() + timezone.timedelta(weeks=1) >= self.date and self.can_join()
 
