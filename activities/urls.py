692eb43ecd52dac85a3a6e7f005918e531852296
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-18 23:11:23 +0700

    Remove path to old_views accept join

--- activities/urls.py
+++ activities/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path("", views.ActivityList.as_view(), name="index"),
     path("<int:activity_id>/join", old_views.join, name="join"),
     path("<int:pk>", views.ActivityDetail.as_view(), name="detail"),
-    # path("create", views.create, name="create"),
-    path("<int:activity_id>/edit", old_views.edit_activity, name="edit_activity"),
 
     # Utilities.
     path('get-csrf-token/', views.util.csrf_token_view, name='get_csrf_token'),
