61a6caf11dcaf95709036c0c4c0572399cfc6696
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-10-20 17:25:39 +0700

    Remove unnecessary permission classes and put in the detail post method

--- activities/views/activity_detail.py
+++ activities/views/activity_detail.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 from django.utils import timezone
 from rest_framework import generics, permissions, mixins, response, status
 from activities import models, serializers
-from activities.permissions import IsHostOrReadOnly, IsNotJoinedForPOST, IsFullForPOST
+from activities.permissions import IsHostOrReadOnly
 
 
 class ActivityDetail(mixins.RetrieveModelMixin,
@@ -15,7 +15,7 @@ class ActivityDetail(mixins.RetrieveModelMixin,
 
     queryset = models.Activity.objects.filter(date__gte=timezone.now())
     serializer_class = serializers.ActivitiesSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsHostOrReadOnly, IsNotJoinedForPOST, IsFullForPOST]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsHostOrReadOnly]
 
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> response.Response:
         """Handle get request by return detail of an activity."""
@@ -47,8 +47,24 @@ class ActivityDetail(mixins.RetrieveModelMixin,
 
         res_dict = res.data
 
+        activity = models.Activity.objects.get(pk=res_dict.get("id"))
+
+        if not activity.can_join():
+            return response.Response(
+                {"message": f"The activity {res_dict.get('name')} is full.",
+                 "id": activity.id
+                 }, status=401
+            )
+
+        if activity.attend_set.filter(user=request.user).exists():
+            return response.Response(
+                {"message": f"You've already joined the activity {res_dict.get('name')}.",
+                 "id": activity.id
+                 }, status=401
+            )
+
         request.user.attend_set.create(
-            activity=models.Activity.objects.get(pk=res_dict.get("id")),
+            activity=activity,
             is_host=False
         )
 
