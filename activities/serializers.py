60862f2ca6ce8a04ee63a4a920307bc0bd1cd911
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-21 23:47:07 +0700

    Implement custom validator class

--- activities/serializers.py
+++ activities/serializers.py
@@ -4,6 +4,36 @@ from typing import Any
 from rest_framework import serializers, validators
 from . import models
 
+def can_join_validator(attrs, *args, **kwargs) -> serializers.ValidationError | None:
+    act = attrs.get('activity')
+
+    if not act.can_join():
+        message = f'The activity {act.name} is full.'
+        raise serializers.ValidationError(message)
+    
+class CustomMsgUniqueTogetherValidator(validators.UniqueTogetherValidator):
+    
+    def __call__(self, attrs, serializer):
+        self.enforce_required_fields(attrs, serializer)
+        queryset = self.queryset
+        queryset = self.filter_queryset(attrs, queryset, serializer)
+        queryset = self.exclude_current_instance(attrs, queryset, serializer.instance)
+
+        # Ignore validation if any field is None
+        if serializer.instance is None:
+            checked_values = [
+                value for field, value in attrs.items() if field in self.fields
+            ]
+        else:
+            # Ignore validation if all field values are unchanged
+            checked_values = [
+                value
+                for field, value in attrs.items()
+                if field in self.fields and value != getattr(serializer.instance, field)
+            ]
+
+        if checked_values and None not in checked_values and validators.qs_exists(queryset):
+            raise validators.ValidationError({"message": f"You've already joined the activity {attrs.get('activity').name}."}, code='unique')
 
 class ActivitiesSerializer(serializers.ModelSerializer):
     """Serialized activity."""
@@ -36,8 +66,10 @@ class AttendSerializer(serializers.ModelSerializer):
         fields = ('__all__')
         
         validators = [
-            validators.UniqueTogetherValidator(
+            CustomMsgUniqueTogetherValidator(
                 queryset=model.objects.all(),
                 fields=['user', 'activity']
-            )
+            ),
+            can_join_validator
         ]
+
