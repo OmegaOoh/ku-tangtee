da45300b39bf7f41639f46b92d067037d42eb5ea
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-10-23 09:11:38 +0700

    Extract keyword search method from get to get_queryset

--- activities/views/activity_list.py
+++ activities/views/activity_list.py
@@ -16,19 +16,21 @@ class ActivityList(
 ):
     """Return list of available upcoming activity when GET request and create new activity when POST request."""
 
-    queryset = models.Activity.objects.filter(date__gte=timezone.now()).order_by("date")
     serializer_class = serializers.ActivitiesSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def get_queryset(self):
+        """Activity index view returns a list of all the activities according to query parameters."""
+        queryset = models.Activity.objects.filter(date__gte=timezone.now()).order_by("date")
+
+        keyword = self.request.GET.get("keyword")
+        if keyword:
+            queryset = queryset.filter(Q(name__iregex=rf'{keyword}') | Q(detail__iregex=rf'{keyword}'))
+
+        return queryset
+
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> response.Response:
         """Handle get request by return with list of activity."""
-        keyword = request.GET.get("keyword")
-        if keyword:
-            activity = models.Activity.objects.filter(Q(name__iregex=rf'{keyword}') | Q(detail__iregex=rf'{keyword}'),
-                                                      date__gte=timezone.now()).order_by("date")
-            serializer = serializers.ActivitiesSerializer(activity, many=True)
-            return response.Response(serializer.data)
-
         return self.list(request, *args, **kwargs)
 
     def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> response.Response:
