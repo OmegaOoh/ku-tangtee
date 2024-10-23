e6eb00e5f90d65f1117c81199209c5b658d1005f
Sirapob Phasut <omegaooh@gmail.com>
2024-10-23 11:46:35 +0700

    Update Visual of Index page

--- frontend/src/views/ActivityIndex.vue
+++ frontend/src/views/ActivityIndex.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class='container mx-auto p-4'>
-        <h1 class='text-4xl font-bold mb-4'>Activities List</h1>
-        <div id='reload' style='padding: 1%;' hidden>
-            <button class ='btn btn-accent' @click='fetchActivities()'>
-                New Activity Available!, Reload Now
+    <div class="fixed top-16 left-0 right-0 flex justify-center z-10" style="padding: 1%;">
+        <div id="reload" class="transform translate-y-0 transition-transform duration-300 ease-in-out" hidden>
+            <button class='btn btn-accent size-fit text-xl' @click='fetchActivities()'>
+                ↻ Refresh
             </button>
         </div>
+    </div>
+    <div class='container mx-auto p-4'>
+        <h1 class='text-4xl font-bold mb-4 flex justify-center'>Activities List</h1>
         <div v-if='activities.length'>
             <div class='grid grid-cols-1 md:grid-cols-2 gap-4'>
                 <div
                     v-for='activity in activities'
                     :key='activity.id'
-                    class='card bg-base-100 shadow-lg hover:shadow-xl transition-shadow duration-200'
+                    class='card bg-base-300 hover:border-primary border-2 border-base-300 shadow-lg transition-all duration-150'
                 >
                     <div
-                        class='bg-primary card-body p-4'
+                        class='card-body p-4 '
                         style='border-radius: 8px'
                     >
-                        <h2 class='card-title text-2xl font-semibold'>
+                        <h2 class='card-title text-2xl font-semibold overflow-hidden whitespace-nowrap text-ellipsis'>
                             {{ activity.name }}
                         </h2>
-                        <p class='line-clamp-2'>{{ activity.detail }}</p>
+                        <p class='line-clamp-2 text-ellipsis'>{{ activity.detail }}</p>
                         <p>
                             Start date:
                             {{ formatActivityDate(activity.date) }}
@@ -30,7 +32,7 @@
                                 :to='{ path: `/activities/${activity.id}` }'
                             >
                                 <button
-                                    class='btn btn-info'
+                                    class='btn btn-secondary'
                                     @click='viewActivity(activity.id)'
                                 >
                                     View
@@ -41,9 +43,18 @@
                 </div>
             </div>
         </div>
-        <p v-else class='text-center text-gray-500 mt-4'>
-            No upcoming activities found.
-        </p>
+        <div v-else class='mt-4'>
+            <div class="card bg-base-300 border-2 border-accent p-5">
+                <div class="card-title text-2xl font-semibold">
+                    No upcoming activities found.
+                </div>
+                <div class='card-actions justify-end'>
+                    <router-link to='/create'>
+                        <button class="btn btn-accent">Create New!</button>
+                    </router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -94,7 +105,13 @@ export default {
             try {
                 const response = await apiClient.get('/activities/'); // Trying to get data from API
                 this.activities = response.data;
-                document.getElementById('reload').setAttribute('hidden', true);
+                window.scrollTo(0,0);
+                // Hide reload button
+                const reloadButton = document.getElementById('reload')
+                reloadButton.classList.remove('translate-y-0');
+                reloadButton.classList.remove('translate-y-[100%]');
+                setTimeout(reloadButton.setAttribute('hidden', 'true'));
+
             } catch (error) {
                 console.error('Error fetching activities:', error);
                 if (error.response) {
@@ -130,10 +147,13 @@ export default {
             socket.onmessage = (event) => {
                     try {
                         var parsedData = JSON.parse(event.data);
-                        if (parsedData['type'] === 'new_act')
-                            {
-                                document.getElementById('reload').removeAttribute('hidden')
-                            }
+                        if (parsedData['type'] === 'new_act') {
+                                // Show reload button
+                                const reloadButton = document.getElementById('reload');
+                                reloadButton.removeAttribute('hidden'); // Show the button
+                                reloadButton.classList.remove('translate-y-[-100%]'); // Remove off-screen class
+                                reloadButton.classList.add('translate-y-0'); // Slide in
+                        }
                     } catch (error) {
                         console.log('Parsing Error: ', error)
                     }
@@ -142,3 +162,14 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+    .line-clamp-2 {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        line-clamp: 2;
+        word-wrap: normal;
+        overflow: hidden;
+    }
+</style>
