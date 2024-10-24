382bca49f07d5c4617ac488a1e7a11f70991c16b
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-10-24 09:42:38 +0700

    Sphinx style docstring

--- chat/views.py
+++ chat/views.py
@@ -11,6 +11,9 @@ class ChatMessageList(generics.ListAPIView):
     serializer_class = MessageSerializer
 
     def get_queryset(self) -> QuerySet[Message]:
-        """Query chat message by using activity_id."""
+        """Query chat messages using activity id.
+
+        :return: all the messages that exist in the activity
+        """
         activity_id = self.kwargs['activity_id']
         return Message.objects.filter(activity_id=activity_id).order_by('timestamp')
