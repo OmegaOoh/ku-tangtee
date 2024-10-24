7f40e4a455fbef631ecb7e3ee1aeaabc87f82acd
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-24 21:59:09 +0700

    Flake8 correction and remove deadcode

--- auth/serializer.py
+++ auth/serializer.py
@@ -1,20 +1,28 @@
+"""Module contain serializer class relate to User model."""
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from allauth.socialaccount.models import SocialAccount
 
 
 class UserSerializer(serializers.ModelSerializer):
-    
+    """Serialized User data."""
+
     profile_picture_url = serializers.SerializerMethodField()
-    
+
     class Meta:
+        """User serailizer Meta class."""
+
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'profile_picture_url')
-        
-    def get_profile_picture_url(self, obj) -> str:
-        
+
+    def get_profile_picture_url(self, user) -> str:
+        """Get profile picture from provided user.
+
+        :param obj: User model instance
+        :return: String of user profile URL or empty string if user doesn't have social account.
+        """
         try:
-            social_account = SocialAccount.objects.get(user=obj)
+            social_account = SocialAccount.objects.get(user=user)
             profile_picture_url = social_account.extra_data.get('picture', '')
             return profile_picture_url
         except SocialAccount.DoesNotExist:
--- auth/views.py
+++ auth/views.py
@@ -4,8 +4,9 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 from rest_framework import generics, mixins, response
 from .serializer import UserSerializer, SocialAccount
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django.http import HttpRequest
+
 
 class GoogleLogin(SocialLoginView):
     """Social Login View for Google OAuth."""
@@ -14,11 +15,11 @@ class GoogleLogin(SocialLoginView):
 
 
 class UserList(generics.GenericAPIView, mixins.ListModelMixin):
+    """User data view."""
 
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    
+
     def get(self, request: HttpRequest, *args, **kwargs) -> response.Response:
-        
+        """Response GET request by send list of serialized user."""
         return self.list(request, args, kwargs)
-    
