bb309b035bfd6a5d817e7588db05c5fac3baeff5
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-10-25 22:51:32 +0700

    Flake8 correction

--- mysite/views.py
+++ mysite/views.py
@@ -20,6 +20,7 @@ def profile_picture_view(request: HttpRequest) -> response.Response:  # pragma: 
     except SocialAccount.DoesNotExist:
         return response.Response({"error": "Google account not found for user"}, status=404)
 
+
 @decorators.api_view(["GET"])
 def get_user_data(request: HttpRequest, user_id: int) -> response.Response:  # pragma: no cover
     """Return user data from Google account.
@@ -33,8 +34,8 @@ def get_user_data(request: HttpRequest, user_id: int) -> response.Response:  # p
         social_account = SocialAccount.objects.get(user=user)
         profile_picture_url = social_account.extra_data.get('picture', '')
         return response.Response({"profile_picture_url": profile_picture_url,
-                                   "id": user.id,
-                                   "first_name":user.first_name,
-                                   "last_name":user.last_name})
+                                  "id": user.id,
+                                  "first_name": user.first_name,
+                                  "last_name": user.last_name})
     except SocialAccount.DoesNotExist:
         return response.Response({"error": "Google account not found for user"}, status=404)
