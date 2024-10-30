a26e76162c235acf9a2b768e2f049f656bce6b90
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-30 17:33:56 +0700

    Make function to handle participant check-in

--- activities/views/activity_checkin.py
+++ activities/views/activity_checkin.py
@@ -7,10 +7,11 @@ from rest_framework import generics, permissions, mixins, response, status
 from activities import models
 from activities.serializer.permissions import OnlyHostCanEdit
 from activities.serializer import model_serializers
+from . import util
 
 
 class CheckInView(generics.GenericAPIView, 
-                  mixins.UpdateModelMixin):
+                mixins.UpdateModelMixin):
     
     queryset = models.Activity.objects.filter(date__gte=timezone.now())
     serializer_class = model_serializers.ActivitiesSerializer
@@ -28,11 +29,43 @@ class CheckInView(generics.GenericAPIView,
         status = request.GET.get('status', 'no')
         return self.status_change_method.get(status, self.invalid_status)(request, args, kwargs)
     
-    def open_check_in(self, request, *args, **kwargs):
-        request.data['check_in_allowed'] = True
-        res = super().update(request, partial=True,*args, **kwargs)
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> response.Response:
+        code = request.POST.get('check_in_code', 'no')
+        act = self.get_object()
+        
+        if not act.is_participated(request.user):
+            return response.Response(
+                {'message': "You're not member of this activity"}
+            )
+        
+        if not act.check_in_allowed:
+            return response.Response(
+                {'message': 'Check-in are not allow at the moment'}
+            )
+        
+        if not act.verified_check_in_code(code):
+            return response.Response(
+                {'message': 'Check-in code invalid'}
+            )
+        
+        attend = act.attend_set.get(user=request.user)
+        attend.checked_in = True
+        
+        return response.Response(
+            {'message': f"You've successfully check-in to {act.name}"}
+        )
+    
+    def open_check_in(self, request, *args, **kwargs):        
+        request.data.update(
+            {
+                'check_in_allowed': True,
+                'check_in_code': util.get_checkin_code()
+            }
+        )
+        super().update(request, partial=True,*args, **kwargs)
         return response.Response({
-            'message': 'Activity check-in are open'
+            'message': 'Activity check-in are open',
+            'check_in_code': request.data.get('check_in_code')
         })
     
     def close_check_in(self, request, *args, **kwargs):
