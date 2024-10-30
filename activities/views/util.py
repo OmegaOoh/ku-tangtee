c92f714592675f1605ad10852d0082cecb37b0f5
Pasu Sangiemsin <opxz7148@gmail.com>
2024-10-30 17:32:39 +0700

    Implement function to generate random check-in code

--- activities/views/util.py
+++ activities/views/util.py
@@ -8,7 +8,10 @@ from django.conf import settings
 from activities import models
 from rest_framework import decorators, response
 from rest_framework.permissions import IsAuthenticated
+import random
+import string
 
+CHECKIN_CODE_LEN = 6
 
 @decorators.api_view(['get'])
 def csrf_token_view(request: HttpRequest) -> JsonResponse:  # pragma: no cover
@@ -29,3 +32,11 @@ def get_recent_activity(request: HttpRequest) -> JsonResponse:  # pragma: no cov
     activities = models.Attend.recently_joined(user)
     recent_activities = [{"name": activity.name, "activity_id": activity.id} for activity in activities]
     return response.Response(recent_activities)
+
+
+def get_checkin_code():
+    # choose from all lowercase letter
+    letters = string.ascii_uppercase
+    result_str = ''.join(random.choice(letters) for i in range(CHECKIN_CODE_LEN))
+    return result_str
+
