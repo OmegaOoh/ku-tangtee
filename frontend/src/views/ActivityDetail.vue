bf5935169b8ab1777cacd37af64e88295cf36ec1
Pasu Sangiemsin <opxz7148@gmail.com>
2024-11-01 22:48:51 +0700

    Make host able to show check in code as a modal

--- frontend/src/views/ActivityDetail.vue
+++ frontend/src/views/ActivityDetail.vue
@@ -34,14 +34,20 @@
             :class="{ 'modal-open': showCheckInCode }"
         >
             <div class="modal-box shadow-xl">
-                <div class="sticky flex justify-end">
-                    <button
-                        class="btn btn-ghost btn-circle"
-                        @click="closeCheckInCodeModal"
-                    >
-                        x
-                    </button>
-                </div>
+                <button
+                    class="btn btn-ghost btn-circle absolute right-6 top-6 hover:text-error"
+                    @click="closeCheckInCodeModal"
+                >
+                    <svg
+                        class="swap-on fill-current"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 512 512">
+                        <polygon
+                        points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                    </svg>
+                </button>
                 <CheckInModal
                     @update-success="
                         () => {
@@ -78,6 +84,13 @@
                         class="btn btn-ghost text-accent ml-2 mr-2"
                     >
                         Close Checkin
+                    </button>
+                    <button
+                        v-if="isHost && activity.check_in_allowed && isAuth"
+                        @click="openCheckInCodeModal"
+                        class="btn btn-ghost text-accent ml-2 mr-2"
+                    >
+                        Show Check-In code
                     </button>
                 </h1>
                 <p class="mb-2 ml-3 overflow-hidden multi-line">
