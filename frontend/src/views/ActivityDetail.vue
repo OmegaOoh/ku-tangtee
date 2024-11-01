9088506a50ac23ce38b32fc3ae490b3642bab856
Sirapob Phasut <omegaooh@gmail.com>
2024-11-01 22:32:51 +0700

    change X marks on edit modal

--- frontend/src/views/ActivityDetail.vue
+++ frontend/src/views/ActivityDetail.vue
@@ -13,10 +13,18 @@
             <div class="modal-box shadow-xl">
                 <div class="sticky flex justify-end">
                     <button
-                        class="btn btn-ghost btn-circle"
+                        class="btn btn-ghost btn-circle absolute hover:text-error"
                         @click="closeModal"
                     >
-                        x
+                        <svg
+                            class="swap-on fill-current"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="32"
+                            height="32"
+                            viewBox="0 0 512 512">
+                            <polygon
+                            points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                        </svg>
                     </button>
                 </div>
                 <EditModal
@@ -65,7 +73,7 @@
                     <div
                         v-for="participant in people"
                         :key="participant.id"
-                        class="card bg-base-100 shadow-lg p-4 rounded-lg border-primary hover: border-2 transition-all duration-75 ease-in-out"
+                        class="card bg-base-100 shadow-lg p-4 rounded-lg border-primary hover:border-2 cursor-pointer transition-all duration-75 ease-in-out"
                         @click="$router.push('/profile/'+ participant.username)"
                     >
                         <div class="flex items-center space-x-4">
