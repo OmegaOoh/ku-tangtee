2572196e9d8b1d34f6b46c7f1f980f8ca3a80516
Sirapob Phasut <omegaooh@gmail.com>
2024-11-05 22:04:02 +0700

    Move scripts in CheckInCodeModal out of export default

--- frontend/src/component/CheckInCodeModal.vue
+++ frontend/src/component/CheckInCodeModal.vue
@@ -1,13 +1,13 @@
 <template>
-    <div  v-if="checkInAllowed">
+    <div  v-if="activity.check_in_allowed">
         <h1 @allow-checked-in="fetchCheckInCode" class="card-title text-4xl mr-2 text-base-content align-center">Check-In code: </h1>
         <br>
-        <h1 class="card-title text-8xl mr-2 align-center text-accent">{{ this.activity.check_in_code }}</h1>
+        <h1 class="card-title text-8xl mr-2 align-center text-accent">{{ activity.check_in_code }}</h1>
         <br>
         <button 
             @click="closeCheckIn"
             type="button" 
-            class="btn btn-error"
+            class="btn btn-error hover:brightness-50"
         >
             Close Check-in
         </button>
@@ -22,96 +22,95 @@
     </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted, defineEmits } from 'vue'
+import { useRoute } from 'vue-router'
 import apiClient from "@/api";
 import { addAlert } from "@/functions/AlertManager";
 import { createPutRequest } from "@/functions/HttpRequest.js";
 
-export default {
-    data() {
-        return {
-            id: this.activityId,
-            activity: {},
-            checkInCode: "",
-            checkInAllowed: false
-        };
-    },
-    methods: {
-        async fetchCheckInCode() {
-            /*
-             * Get data from specific activity including participant detail.
-             */
-            try {
-                const response = await apiClient.get(
-                    `/activities/${this.activityId}`
-                );
-                this.activity = response.data;
-                this.checkInCode = this.activity.check_in_code
-                this.checkInAllowed = this.activity.check_in_allowed
-            } catch (error) {
-                console.error("Error fetching activity:", error);
-                addAlert(
-                    "warning",
-                    "Activity already started or No such activity."
-                );
-            }
-        },
-        async allowCheckIn() {
-            /*
-             * Attempt to join activity.
-             */
-            try {
-                const response = await createPutRequest(
-                    `/activities/check-in/${this.activityId}/?status=open`,
-                    {}
-                );
-                addAlert("success", response.data.message);
-                this.$emit("allow-checked-in")
-                this.fetchCheckInCode();
-            } catch (error) {
-                if (error.response && error.response.data) {
-                    addAlert("error", error.response.data.message); // Show error message from backend
-                } else {
-                    addAlert(
-                        "error",
-                        "An unexpected error occurred. Please try again later."
-                    );
-                }
-            }
-        },
-        async closeCheckIn() {
-            /**
-             * Make check-in unavailable.
-             */
-            // 
-            try {
-                const response = await createPutRequest(
-                    `/activities/check-in/${this.activityId}/?status=close`,
-                    {}
-                );
-                addAlert("success", response.data.message);
-                this.$emit("closed-checked-in")
-                this.fetchCheckInCode(); //Fetch Activity
-            } catch (error) {
-                console.error("Error fetching activity:", error);
-                addAlert(
-                    "warning",
-                    "Activity already started or No such activity."
-                );
-            }
-        },
-    },
-    mounted() {
-        this.activityId = this.$route.params.id;
-        this.isDarkTheme = window.matchMedia(
-            "(prefers-color-scheme: dark)"
-        ).matches;
-        window
-            .matchMedia("(prefers-color-scheme: dark)")
-            .addEventListener("change", (e) => {
-                this.isDarkTheme = e.matches;
-            });
-        this.fetchCheckInCode();
-    },
-};
+const emit = defineEmits(["allow-checked-in", "closed-checked-in"]) 
+const route = useRoute();
+
+const activityId = ref(0);
+const activity = ref({});
+const isDarkTheme = ref(true);
+
+async function fetchCheckInCode() {
+    /**
+    * Get data from specific activity including participant detail.
+    */
+    try {
+    const response = await apiClient.get(
+        `/activities/${activityId.value}`
+    );
+    activity.value = response.data;
+    } catch (error) {
+        console.error("Error fetching activity:", error);
+        addAlert(
+            "warning",
+            "Activity already started or No such activity."
+        );
+    }
+}
+
+async function allowCheckIn() {
+    /*
+        * Attempt to join activity.
+        */
+    try {
+        const response = await createPutRequest(
+            `/activities/check-in/${activityId.value}/?status=open`,
+            {}
+        );
+        addAlert("success", response.data.message);
+        emit("allow-checked-in")
+        fetchCheckInCode();
+    } catch (error) {
+        if (error.response && error.response.data) {
+            addAlert("error", error.response.data.message); // Show error message from backend
+        } else {
+            console.error(error)
+            addAlert(
+                "error",
+                "An unexpected error occurred. Please try again later."
+            );
+        }
+    }
+}
+
+async function closeCheckIn() {
+    /**
+     * Make check-in unavailable.
+     */
+    try {
+        const response = await createPutRequest(
+            `/activities/check-in/${activityId.value}/?status=close`,
+            {}
+        );
+        addAlert("success", response.data.message);
+        emit("closed-checked-in")
+        fetchCheckInCode(); //Fetch Activity
+    } catch (error) {
+        console.error("Error fetching activity:", error);
+        addAlert(
+            "warning",
+            "Activity already started or No such activity."
+        );
+    }   
+}
+
+onMounted(() => {
+    activityId.value = route.params.id;
+    isDarkTheme.value = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+    ).matches;
+    window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", (e) => {
+            isDarkTheme.value = e.matches;
+        });
+    fetchCheckInCode();
+})
+
 </script>
