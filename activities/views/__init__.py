82f8d0a253fd55ecb783bab3a235893e1d5a0559
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-11-06 11:54:08 +0700

    Remove activity host view

--- activities/views/__init__.py
+++ activities/views/__init__.py
@@ -2,6 +2,5 @@ from .activity_list import ActivityList
 from .activity_detail import ActivityDetail
 from .activity_join import JoinLeaveView
 from .activity_checkin import CheckInView
-from .activity_host import GrantRemoveHostView
 
 from .util import *
