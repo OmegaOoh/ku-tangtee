06346553db285bfa865a4edf0804933f59c009f8
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-11-10 14:59:53 +0700

    Mypy correction

--- profiles/logger.py
+++ profiles/logger.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 logger = logging.getLogger('profiles')
 
 
-def convert_to_log_msg(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = None):
+def convert_to_log_msg(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = '') -> str:
     """Convert parameters into log message.
 
     :param req_user: request user
@@ -15,15 +15,15 @@ def convert_to_log_msg(req_user: User, action: str, profile_id: int = -1, target
     :param target_user: target user of the action
     :param reason: reason or note of the action
     """
-    req_user_str: str = f'User {req_user.id} ({req_user.first_name})'
-    profile_str: str = f'Profile {profile_id}' if profile_id != -1 else 'Profile'
-    target_user: str = f' User {target_user.id} ({target_user.first_name})' if target_user else ''
-    reason: str = f' ({reason})' if reason else ''
+    req_user_str = f'User {req_user.id} ({req_user.first_name})'
+    profile_str = f'Profile {profile_id}' if profile_id != -1 else 'Profile'
+    target_user = f' User {target_user.id} ({target_user.first_name})' if target_user else ''
+    reason = f' ({reason})' if reason else ''
 
     return f'{req_user_str} {action}{target_user} {profile_str}{reason}'
 
 
-def info(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = None):
+def info(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = '') -> None:
     """Log INFO level messages in profiles app.
 
     :param req_user: request user
@@ -36,7 +36,7 @@ def info(req_user: User, action: str, profile_id: int = -1, target_user: User = 
     logger.info(msg)
 
 
-def warning(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = None):
+def warning(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = '') -> None:
     """Log WARNING level messages in profiles app.
 
     :param req_user: request user
