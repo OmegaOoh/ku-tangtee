edc4c3e614e0ab2d4c194b25abbb7ce2aac4bd6a
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-11-11 15:14:46 +0700

    Revise profiles app logging behaviour

--- profiles/logger.py
+++ profiles/logger.py
@@ -1,49 +1,46 @@
 """logger module for logging profiles app."""
 import logging
+from dataclasses import dataclass
+from enum import Enum
 from django.contrib.auth.models import User
 
 
 logger = logging.getLogger('profiles')
 
 
-def convert_to_log_msg(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = '') -> str:
+class Action(Enum):
+    """Enum class that store a format string template that contain action."""
+
+    CREATE = "{} CREATE{} {}{}"
+    EDIT = "{} EDIT{} {}{}"
+    FAIL_CREATE = "{} FAIL to CREATE{} {}{}"
+    TRY_EDIT = "{} TRY to EDIT{} {}{}"
+
+
+@dataclass
+class RequestData:
+    """Data class that stores user, target user, and profile data from request action.
+
+    :param req_user: request user
+    :param profile_id: activity id, -1 if not specify
+    :param target_user: target user of the action
+    """
+
+    req_user: User
+    profile_id: int
+    target_user: User = None
+
+
+def data_to_log(action: Action, data: RequestData, reason: str = '') -> str:
     """Convert parameters into log message.
 
-    :param req_user: request user
     :param action: action of the request user
-    :param profile_id: profile id, -1 if not specify
-    :param target_user: target user of the action
+    :param data: the request user, target user, and activity data from request action
     :param reason: reason or note of the action
     """
-    req_user_str = f'User {req_user.id} ({req_user.first_name})'
-    profile_str = f'Profile {profile_id}' if profile_id != -1 else 'Profile'
-    target_user = f' User {target_user.id} ({target_user.first_name})' if target_user else ''
+    req_user_str = f'User {data.req_user.id} ({data.req_user.first_name})'
+    act_str = f'Profile {data.profile_id}' if data.profile_id != -1 else 'Profile'
+    target_user = f' User {data.target_user.id} ({data.target_user.first_name})' if data.target_user else ''
     reason = f' ({reason})' if reason else ''
 
-    return f'{req_user_str} {action}{target_user} {profile_str}{reason}'
-
-
-def info(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = '') -> None:
-    """Log INFO level messages in profiles app.
-
-    :param req_user: request user
-    :param action: action of the request user
-    :param profile_id: profile id, -1 if not specify
-    :param target_user: target user of the action
-    :param reason: reason or note of the action
-    """
-    msg = convert_to_log_msg(req_user, action, profile_id, target_user, reason)
-    logger.info(msg)
-
-
-def warning(req_user: User, action: str, profile_id: int = -1, target_user: User = None, reason: str = '') -> None:
-    """Log WARNING level messages in profiles app.
-
-    :param req_user: request user
-    :param action: action of the request user
-    :param profile_id: profile id, -1 if not specify
-    :param target_user: target user of the action
-    :param reason: reason or note of the action
-    """
-    msg = convert_to_log_msg(req_user, action, profile_id, target_user, reason)
-    logger.warning(msg)
+    return action.value.format(req_user_str, target_user, act_str, reason)
