28e435aeaeb0f219191d2e94227f5f78501d8650
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-11-11 15:13:50 +0700

    Revise activities app logging behaviour

--- activities/logger.py
+++ activities/logger.py
@@ -1,49 +1,57 @@
 """logger module for logging activities app."""
 import logging
+from dataclasses import dataclass
+from enum import Enum
 from django.contrib.auth.models import User
 
 
 logger = logging.getLogger('activities')
 
 
-def convert_to_log_msg(req_user: User, action: str, act_id: int, target_user: User = None, reason: str = '') -> str:
-    """Convert parameters into log message.
+class Action(Enum):
+    """Enum class that store a format string template that contain action."""
+
+    CREATE = "{} CREATE{} {}{}"
+    JOIN = "{} JOIN{} {}{}"
+    LEAVE = "{} LEAVE{} {}{}"
+    EDIT = "{} EDIT{} {}{}"
+    KICK = "{} KICK{} {}{}"
+    EDIT_HOST = "{} EDIT HOST ACCESS to{} {}{}"
+    CHECKIN = "{} CHECK-IN to{} {}{}"
+    OPEN_CHECKIN = "{} OPEN CHECK-IN for{} {}{}"
+    CLOSE_CHECKIN = "{} CLOSE CHECK-IN for{} {}{}"
+    FAIL_CREATE = "{} FAIL to CREATE{} {}{}"
+    FAIL_JOIN = "{} FAIL to JOIN{} {}{}"
+    FAIL_EDIT = "{} FAIL to EDIT{} {}{}"
+    FAIL_EDIT_HOST = "{} FAIL to EDIT HOST ACCESS to{} {}{}"
+    FAIL_CHECKIN = "{} FAIL to CHECK-IN to{} {}{}"
+    FAIL_OPEN_CHECKIN = "{} FAIL to OPEN CHECK-IN for{} {}{}"
+
+
+@dataclass
+class RequestData:
+    """Data class that stores user, target user, and activity data from request action.
 
     :param req_user: request user
-    :param action: action of the request user
     :param act_id: activity id, -1 if not specify
     :param target_user: target user of the action
+    """
+
+    req_user: User
+    act_id: int
+    target_user: User = None
+
+
+def data_to_log(action: Action, data: RequestData, reason: str = '') -> str:
+    """Convert parameters into log message.
+
+    :param action: action of the request user
+    :param data: the request user, target user, and activity data from request action
     :param reason: reason or note of the action
     """
-    req_user_str = f'User {req_user.id} ({req_user.first_name})'
-    act_str = f'Activity {act_id}' if act_id != -1 else 'Activity'
-    target_user = f' User {target_user.id} ({target_user.first_name})' if target_user else ''
+    req_user_str = f'User {data.req_user.id} ({data.req_user.first_name})'
+    act_str = f'Activity {data.act_id}' if data.act_id != -1 else 'Activity'
+    target_user = f' User {data.target_user.id} ({data.target_user.first_name})' if data.target_user else ''
     reason = f' ({reason})' if reason else ''
 
-    return f'{req_user_str} {action}{target_user} {act_str}{reason}'
-
-
-def info(req_user: User, action: str, activity_id: int = -1, target_user: User = None, reason: str = '') -> None:
-    """Log INFO level messages in activities app.
-
-    :param req_user: request user
-    :param action: action of the request user
-    :param activity_id: activity id, -1 if not specify
-    :param target_user: target user of the action
-    :param reason: reason or note of the action
-    """
-    msg = convert_to_log_msg(req_user, action, activity_id, target_user, reason)
-    logger.info(msg)
-
-
-def warning(req_user: User, action: str, activity_id: int = -1, target_user: User = None, reason: str = '') -> None:
-    """Log WARNING level messages in activities app.
-
-    :param req_user: request user
-    :param action: action of the request user
-    :param activity_id: activity id, -1 if not specify
-    :param target_user: target user of the action
-    :param reason: reason or note of the action
-    """
-    msg = convert_to_log_msg(req_user, action, activity_id, target_user, reason)
-    logger.warning(msg)
+    return action.value.format(req_user_str, target_user, act_str, reason)
