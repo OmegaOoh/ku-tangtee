48ed198a6084ae45d4a8812052bbe9fa1cb4948c
Thanawas Sirilertsathit <146911997+Thanawas-Sirilertsathit@users.noreply.github.com>
2024-11-24 10:47:52 +0700

    Changed asgi..py by creating asgi_app before import channel

--- backend/mysite/asgi.py
+++ backend/mysite/asgi.py
@@ -10,13 +10,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels import routing, auth
-from chat import routing as chat_routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+django_asgi_app = get_asgi_application()
+
+from channels import routing, auth
+from chat import routing as chat_routing
+
 application = routing.ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": auth.AuthMiddlewareStack(
         routing.URLRouter(
             chat_routing.websocket_urlpatterns
